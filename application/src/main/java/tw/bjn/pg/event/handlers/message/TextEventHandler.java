b7de2b1ac075a89661dee05c1348d1d91624a670
YNC77 <ginger2033@gmail.com>
2019-07-28 15:30:17 +0800

    fix merge error

--- application/src/main/java/tw/bjn/pg/event/handlers/message/TextEventHandler.java
+++ application/src/main/java/tw/bjn/pg/event/handlers/message/TextEventHandler.java
@@ -4,12 +4,51 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import tw.bjn.pg.interfaces.event.EventHandler;
+import tw.bjn.pg.utils.MsgUtils;
+import tw.bjn.pg.utils.YamlReader;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@Slf4j
+@Component
 public class TextEventHandler extends EventHandler<MessageEvent<TextMessageContent>> {
+
+    private MsgUtils msgUtils;
+
+    @Autowired
+    TextEventHandler(MsgUtils msgUtils) {
+        this.msgUtils = msgUtils;
+    }
 
     @Override
     public Message onEvent(MessageEvent<TextMessageContent> event) {
-        return new TextMessage(event.getMessage().getText());
+        return msgUtils.createTextMsg(getReplySentence());
+    }
+
+    public Map<String, List<String>> getQuotations() {
+        Map<String, List<String>> mQuotations = null;
+        try{
+            YamlReader<Map<String, List<String>>> reader = new YamlReader<>();
+            mQuotations = reader.parse("quotations.yml");
+        } catch (IOException e) {
+            log.error("幹",e);
+        }
+
+        return mQuotations;
+    }
+
+    public String getReplySentence() {
+        Map<String, List<String>> mQuotations = getQuotations();
+        Random random = new Random();
+        int replyIndex = random.nextInt(3);
+
+        return mQuotations.get("replySentences").get(replyIndex);
     }
 }
--- application/src/main/java/tw/bjn/pg/utils/MsgUtils.java
+++ application/src/main/java/tw/bjn/pg/utils/MsgUtils.java
@@ -2,7 +2,6 @@ package tw.bjn.pg.utils;
 
 import com.linecorp.bot.model.message.TextMessage;
 import org.springframework.stereotype.Component;
-import tw.bjn.pg.event.handlers.TextEventHandler;
 
 @Component
 public class MsgUtils {
